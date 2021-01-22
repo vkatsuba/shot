7156d15c035c1c715e79be7014d7474f12e644c3
vkatsuba <v.katsuba.dev@gmail.com>
2021-01-22 21:04:29 +0200

    Add License

--- src/shot_sup.erl
+++ src/shot_sup.erl
@@ -1,16 +1,40 @@
-%%% ==================================================================
-%%% @doc shot top level supervisor.
-%%% @end
-%%% ==================================================================
+%% MIT License
+
+%% Copyright (c) 2021 Viacheslav Katsuba <v.katsuba.dev@gmail.com>
+
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+
+%% The above copyright notice and this permission notice shall be included in all
+%% copies or substantial portions of the Software.
+
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%% SOFTWARE.
 
 -module(shot_sup).
 
 -behaviour(supervisor).
 
-%% API
+%%% ==================================================================
+%%% API
+%%% ==================================================================
+
 -export([start_link/0]).
 
-%% Supervisor callbacks
+%%% ==================================================================
+%%% Supervisor callbacks
+%%% ==================================================================
+
+
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
@@ -26,10 +50,6 @@ start_link() ->
 %% Supervisor callbacks
 %%====================================================================
 
-%% Child :: #{id => Id, start => {M, F, A}}
-%% Optional keys are restart, shutdown, type, modules.
-%% Before OTP 18 tuples must be used to specify a child. e.g.
-%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
     {ok, { {one_for_all, 0, 1}, []} }.
 
